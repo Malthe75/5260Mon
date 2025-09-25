b6bac05127c7f30e3c66d10fda9ee5e4f124eb7f
Malthe75 <Malthehf@gmail.com>
2025-09-25 20:12:24 +0200

    start gui with four moves

--- src/sockets/GameSocket.ts
+++ src/sockets/GameSocket.ts
@@ -8,10 +8,16 @@ export class GameSocket {
     this.io.on('connection', (socket) => {
       console.log('A player connected')
 
-      socket.on('playerMove', (data: { action: unknown }) => {
+      socket.on('player1Move', (data: { action: string }) => {
         console.log('Move received:', data)
         // Simple game logic example:
         this.io.emit('gameUpdate', { currentPlayer: 'Player 2', lastMove: data.action })
+      })
+
+      socket.on('player2Move', (data: { action: string }) => {
+        console.log('Move received:', data)
+        // Simple game logic example:
+        this.io.emit('gameUpdate', { currentPlayer: 'Player 1', lastMove: data.action })
       })
     })
   }
